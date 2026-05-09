1dd748daf69caa6c8e07942de8f7ae260fb4de77
Ananya Singh <singhananya5040@gmail.com>
2026-05-09 14:54:57 +0530

    api-error

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://planora-83ve.onrender.com",
+  baseURL: "https://planora-83ve.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
