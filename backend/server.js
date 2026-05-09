68d2d8e146f63671f1dd2182dd8271e7cfa3b9b1
Ananya Singh <singhananya5040@gmail.com>
2026-05-09 14:52:26 +0530

    re-cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost:5173", "https://planora-orcin.vercel.app/login"],
+  origin: ["http://localhost:5173", "https://planora-orcin.vercel.app"],
   credentials: true,
 }));
 
